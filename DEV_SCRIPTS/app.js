d7076ae1f44a44f83833ddc8c02419b789a81312
Dan Moneypenny <catullus1973@gmail.com>
2019-05-20 17:05:37 -0700

    add additional functions

--- DEV_SCRIPTS/app.js
+++ DEV_SCRIPTS/app.js
@@ -1,21 +1,47 @@
 (function imgEnlarge() {
 
-	const handleFigures = () => {
+	const sortFigures = () => {
 		const allFigures = document.querySelectorAll('figure');
 
 		allFigures.forEach((figure, index) => {
 			if(figure.querySelector('.figure_name')) {
-				console.log(index, " is art");
+				let image = figure.querySelector('img');
+				handleImage(image);
 			} else {
-				console.log(index, " is NOT art");
+				return;
 			}
 		});
+
 	};
+
+	const imgEnlarge = (evt, imgObj) => {
+		let imgAttrs = {
+			title: 'click to close image',
+			class: 'img_enlarged'
+		};
+
+		imgAttrs.src = imgObj.getAttribute("src");
+		imgAttrs.alt = imgObj.getAttribute("alt");
+
+		console.log(imgAttrs);
+
+	}
+
+	const handleImage = (imgObj) => {
+		imgObj.style.cursor = ('pointer');
+		imgObj.setAttribute("title", "click to enlarge image");
+		imgObj.addEventListener("click", function(evt) {imgEnlarge(evt, this)}, false);
+	}
+
+
+
+
+	// Initialize 
 
 	const hasFigures = document.querySelectorAll('.figure_name');
 
 	if(hasFigures.length) {
-		handleFigures();
+		sortFigures();
 	} else {
 		console.log('This page does NOT have figures');
 		return;
